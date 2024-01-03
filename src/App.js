5b3e8aa2c87245592a09cf8958d469c9022d9850
John <jhawley318@gmail.com>
2024-01-03 14:17:37 -0800

    changed particle package. removed tsparticles. added debug textfield submission.

--- src/App.js
+++ src/App.js
@@ -1,122 +1,35 @@
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
 import Rank from './components/Rank/Rank.js';
-import Particles from "react-tsparticles";
-import { useCallback } from "react";
-import { loadSlim } from "tsparticles-slim";
+import ParticlesBg from 'particles-bg'
 
-const particleOptions = {
-      background: {
-          color: {
-              value: "none",
-          },
-      },
-      fpsLimit: 120,
-      interactivity: {
-          events: {
-              onClick: {
-                  enable: true,
-                  mode: "push",
-              },
-              onHover: {
-                  enable: true,
-                  mode: "repulse",
-              },
-              resize: true,
-          },
-          modes: {
-            bubble: {
-                  distance: 400,
-                  duration: 2,
-                  opacity: 0.8,
-                  size: 40
-              },
-              push: {
-                  quantity: 4,
-              },
-              repulse: {
-                  distance: 200,
-                  duration: 0.4,
-              },
-          },
-      },
-      particles: {
-          color: {
-              value: "#ffffff",
-          },
-          links: {
-              color: "#ffffff",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-          },
-          collisions: {
-              enable: true
-          },
-          move: {
-              direction: "none",
-              enable: true,
-              outModes: {
-                  default: "bounce",
-              },
-              random: false,
-              speed: 6,
-              straight: false,
-          },
-          number: {
-              density: {
-                  enable: true,
-                  area: 800,
-              },
-              value: 80,
-          },
-          opacity: {
-              value: 0.5,
-          },
-          shape: {
-              type: "circle",
-          },
-          size: {
-            random: true,
-            value: 5
-            // value: { min: 1, max: 5 },
-          },
-      },
-      detectRetina: true,
-}
+class App extends Component {
+    constructor() {
+      super();
+      this.state = {
+        input: '',
+      }
+    }
 
-function App() {
-      const particlesInit = useCallback(async engine => {
-        console.log(engine);
-        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
-        //await loadFull(engine);
-        await loadSlim(engine);
-    }, []);
+    onInputChange = (event) => {
+      console.log(event.target.value);
+    }
 
-    const particlesLoaded = useCallback(async container => {
-        await console.log(container);
-    }, []);
-
-  return (
-    <div className="App">
-    <Particles className='particles'
-            id="tsparticles"
-            init={particlesInit}
-            loaded={particlesLoaded}
-            options={particleOptions}
-        />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRecognition />}*/}
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+      <ParticlesBg color="#FFFFFF" num={100} type="cobweb" bg={true} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange}/>
+        {/*<FaceRecognition />}*/}
+      </div>
+    );
+  }
 }
 
 export default App;
