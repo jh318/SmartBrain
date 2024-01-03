0d7d7276bd0d12e943143dd76e3857995710bf22
John <jhawley318@gmail.com>
2024-01-03 12:05:19 -0800

    init commit. Completed basic react components. About to integrate Clarifai api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,120 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
+import Particles from "react-tsparticles";
+import { useCallback } from "react";
+import { loadSlim } from "tsparticles-slim";
+
+const particleOptions = {
+      background: {
+          color: {
+              value: "none",
+          },
+      },
+      fpsLimit: 120,
+      interactivity: {
+          events: {
+              onClick: {
+                  enable: true,
+                  mode: "push",
+              },
+              onHover: {
+                  enable: true,
+                  mode: "repulse",
+              },
+              resize: true,
+          },
+          modes: {
+            bubble: {
+                  distance: 400,
+                  duration: 2,
+                  opacity: 0.8,
+                  size: 40
+              },
+              push: {
+                  quantity: 4,
+              },
+              repulse: {
+                  distance: 200,
+                  duration: 0.4,
+              },
+          },
+      },
+      particles: {
+          color: {
+              value: "#ffffff",
+          },
+          links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+          },
+          collisions: {
+              enable: true
+          },
+          move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                  default: "bounce",
+              },
+              random: false,
+              speed: 6,
+              straight: false,
+          },
+          number: {
+              density: {
+                  enable: true,
+                  area: 800,
+              },
+              value: 80,
+          },
+          opacity: {
+              value: 0.5,
+          },
+          shape: {
+              type: "circle",
+          },
+          size: {
+            random: true,
+            value: 5
+            // value: { min: 1, max: 5 },
+          },
+      },
+      detectRetina: true,
+}
 
 function App() {
+      const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        //await loadFull(engine);
+        await loadSlim(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Particles className='particles'
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={particleOptions}
+        />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/*<FaceRecognition />}*/}
     </div>
   );
 }
